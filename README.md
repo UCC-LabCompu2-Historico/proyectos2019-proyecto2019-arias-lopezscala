b379f7cb73005601407c7d13da49616a0fd0c67f
faculopezscala <48757769+faculopezscala@users.noreply.github.com>
2019-07-25 18:18:54 -0300

    Add files via upload
    **correccion 25/7**

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # proyectos2019-proyecto2019-arias-lopezscala
 proyectos2019-proyecto2019-arias-lopezscala created by GitHub Classroom
 SITIO WEB PARA SIMULAR CARRERAS ELIGIENDO LOS ATRIBUTOS DE TU AUTO (POTENCIA, PESO Y CALIDAD DE TRANMISION)
-Simplemente elegis los atributos de tu auto y se simula una carrera, en la seccion de ?porque? podes encontrar mas informacion
--- functions.js
+++ functions.js
@@ -1,56 +1,249 @@
+/* LOPEZ SCALA Facundo, ARIAS Lucas - Laboratorio de computacion 2 */
 
-var canvas, ctx, x, y;
+//declaro variables
+var posX1=5, posX2=5, ctx, auto1, auto2;
+let a1, a2, vf1, vf2, t1, t2;
 
-canvas = document.getElementById('canvas');
-ctx = canvas.getContext('2d');
+//Se declaran las imagenes del fondo a utilizar
+pasto1 = new Image;
+pasto1.src = "imagenes/pasto.jpg";
+pasto2 = new Image;
+pasto2.src = "imagenes/pasto.jpg";
+pasto3 = new Image;
+pasto3.src = "imagenes/pasto.jpg";
 
-x = 0;
-y = 0;
-w = 0;
-h = 0;
-
-setInterval(function() {
-
-    ctx.clearRect(0, 0, 1000, 250);
-
-    x = x >= 1000 ? 0 : x + 1;
-    y = 125;
-
-    ctx.fillRect(x, y, 100, 75);
-}, 100 / 24);
-
-setInterval(function() {
-
-    ctx.clearRect(0, 250, 1000, 500);
-
-    w = w >= 1000 ? 0 : w + 1;
-    h = 375;
-
-    ctx.fillRect(w, h, 100, 75);
-}, 100 / 24);
-
-
-/*
-la idea de esta parte de la animacion es importar las fotos, tuvimos problemas importando las fotos y los datos, por lo que simulamos como se veria la carrera con dos rectangulos
-negros a simple modo ilustrativo.
-auto=new Image();
-auto.src='formula_1.png';
-                function dibujarauto()
-                {
-                  var canvas = document.getElementById("canvas");
-                  var ctx = canvas.getContext("2d");
-                  ctx.drawImage(auto,230,30,70,130);
-
-                }
-
-        function anim(){ //Esta es la misma funcion pero solo con el formula 1, la idea es que se pueda seleccionar la foto en base al auto elegido antes por el jugador
-                    //tambien la idea es que las animaciones dependan de la aceleracion calculada antes, no sabemos como traer esos datos
-          ctx.clearRect(0, 0, 600, 200);
-          dibujarauto();
-          if(pos_x  >170) {
-            pos_x = 170;
-            dibujarauto();
-          setTimeout(anim, 10);
+/**
+ * Funcion que calcula los valores de aceleracion, tiempo y velocidad final del auto 1. Tambien selecciona
+ * que el auto mostrado en pantalla coincide con la carroceria elegida
+ * @function calcula1
+ * @param
+ * @return
+ */
+    function calcula1(){
+    var carroc1 = document.getElementById("carroceria1").value;
+    var motor1 = document.getElementById("motor1").value;
+    var transmision1 = document.getElementById("transmision1").value;
+    a1=(9.8*((motor1*7)/carroc1*0.8))*transmision1;
+    t1= Math.sqrt((2*400)/a1);
+    vf1=a1*t1;
+    alert( "La aceleracion del auto 1 es: "+a1+ "[m/s], llega a la meta a "+vf1+"[m/s] y tarda"+t1+"segundos.");
+        if(carroc1==1000){
+        auto1 = new Image;
+        auto1.src = "imagenes/formula_1.png";
+        }
+        if(carroc1==1200){
+        auto1 = new Image;
+        auto1.src = "imagenes/bugatti.png";
+        }
+        if(carroc1==1400){
+        auto1 = new Image;
+        auto1.src = "imagenes/porsche.png";
+        }
+        if(carroc1==1600){
+        auto1 = new Image;
+        auto1.src = "imagenes/masseratti.png";
+        }
+        if(carroc1==1800){
+        auto1 = new Image;
+        auto1.src = "imagenes/rally.png";
+        }
+        if(carroc1==2000){
+        auto1 = new Image;
+        auto1.src = "imagenes/cabriolet.png";
+        }
+        if(carroc1==2200){
+        auto1 = new Image;
+        auto1.src = "imagenes/muscle.png";
+        }
+        if(carroc1==2400){
+        auto1 = new Image;
+        auto1.src = "imagenes/pontiac.png";
+        }
+        if(carroc1==2600){
+        auto1 = new Image;
+        auto1.src = "imagenes/x3.png";
+        }
+        if(carroc1==2800){
+        auto1 = new Image;
+        auto1.src = "imagenes/hummer.png";
+        }
+        if(carroc1==3000){
+        auto1 = new Image;
+        auto1.src = "imagenes/flete.png";
         }
 
-*/
+    }
+
+/**
+ * Funcion que calcula los valores de aceleracion, tiempo y velocidad final del auto 2. Tambien selecciona
+ * que el auto mostrado en pantalla coincide con la carroceria elegida
+ * @function calcula2
+ * @param
+ * @return
+ */
+    function calcula2(){
+    var carroc2 = document.getElementById("carroceria2").value;
+    var motor2 = document.getElementById("motor2").value;
+    var transmision2 = document.getElementById("transmision2").value;
+    a2=(9.8*((motor2*7)/carroc2*0.8))*transmision2;
+    t2= Math.sqrt((2*400)/a2);
+    vf2=a2*t2;
+    alert( "La aceleracion del auto 2 es: "+a2+ "[m/s], llega a la meta a "+vf2+"[m/s] y tarda"+t2+"segundos.");
+
+    if(carroc2==1000){
+        auto2 = new Image;
+        auto2.src = "imagenes/formula_1.png";
+    }
+    if(carroc2==1200){
+        auto2 = new Image;
+        auto2.src = "imagenes/bugatti.png";
+    }
+    if(carroc2==1400){
+        auto2 = new Image;
+        auto2.src = "imagenes/porsche.png";
+    }
+    if(carroc2==1600){
+        auto2 = new Image;
+        auto2.src = "imagenes/masseratti.png";
+    }
+    if(carroc2==1800){
+        auto2 = new Image;
+        auto2.src = "imagenes/rally.png";
+    }
+    if(carroc2==2000){
+        auto2 = new Image;
+        auto2.src = "imagenes/cabriolet.png";
+    }
+    if(carroc2==2200){
+        auto2 = new Image;
+        auto2.src = "imagenes/muscle.png";
+    }
+    if(carroc2==2400){
+        auto2 = new Image;
+        auto2.src = "imagenes/pontiac.png";
+    }
+    if(carroc2==2600){
+        auto2 = new Image;
+        auto2.src = "imagenes/x3.png";
+    }
+    if(carroc2==2800){
+        auto2 = new Image;
+        auto2.src = "imagenes/hummer.png";
+    }
+    if(carroc2==3000){
+        auto2 = new Image;
+        auto2.src = "imagenes/flete.png";
+    }
+    }
+
+/**
+ * Funcion que selecciona que funcion dibujadora del auto llama en base a su acelereacion, para que en el canvas
+ * llegue primero el auto con mas aceleracion, o lleguen juntos en caso de empate
+ * @function selecciona
+ * @param
+ * @return
+ */
+    function selecciona(){
+
+        if(a1>=a2){
+            dibujaauto1();
+            dibujaauto2();
+
+        }
+        if(a1<=a2){
+            dibujaauto1a();
+            dibujaauto2a();
+        }
+
+
+}
+
+/**
+ * Funcion animadora
+ * @function anim
+ * @param
+ * @return
+ */
+    function anim() {
+    ctx.clearRect(0,0, 1000, 500);
+        selecciona();
+        dibujafondo();
+        setTimeout(anim, 20);
+    }
+
+/**
+ * Funcion que dibuja el auto 1 en caso de que tenga mas aceleracion
+ * @function dibujaauto1
+ * @param
+ * @return
+ */
+    function dibujaauto1() {
+            ctx.drawImage(auto1, posX1, 125, 160, 60);
+            if (posX1 > 800)
+                posX1 = 800;
+            else
+                posX1 += 3;
+     }
+
+/**
+ * Funcion que dibuja el auto 2 en caso de que tenga menos aceleracion
+ * @function dibujaauto2
+ * @param
+ * @return
+ */
+    function dibujaauto2(){
+            ctx.drawImage(auto2, posX2, 300, 160, 60);
+            if (posX2 > 800)
+                posX2 = 800;
+            else
+                posX2 += 2;
+     }
+
+/**
+ * Funcion que dibuja el auto 1 en caso de que tenga menos aceleracion
+ * @function dibujaauto1a
+ * @param
+ * @return
+ */
+    function dibujaauto1a() {
+        ctx.drawImage(auto1, posX1, 125, 160, 60);
+        if (posX1 > 800)
+            posX1 = 800;
+        else
+            posX1 += 2;
+        }
+
+/**
+ * Funcion que dibuja el auto 2 en caso de que tenga mas aceleracion
+ * @function dibujaauto2a
+ * @param
+ * @return
+ */
+    function dibujaauto2a(){
+        ctx.drawImage(auto2, posX2, 300, 160, 60);
+        if (posX2 > 800)
+            posX2 = 800;
+        else
+            posX2 += 3;
+        }
+
+/**
+ * Funcion que dibuja el cesped en el fondo
+ * @function dibujafondo
+ * @para
+ * @return
+ */
+    function dibujafondo(){
+        ctx.drawImage(pasto1, 0, 0, 1000, 105);
+        ctx.drawImage(pasto2, 0, 205, 1000, 75);
+        ctx.drawImage(pasto3, 0, 380, 1000, 150);
+        }
+
+
+    window.onload = function() {
+        ctx = document.getElementById("canvas").getContext("2d");
+        anim();
+        };
+
+
+
